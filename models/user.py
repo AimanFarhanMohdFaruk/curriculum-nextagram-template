308cb005a47fd49a854dc7954e65a7eb4574ebb0
Aiman Farhan <aimanfaruk98@gmail.com>
2021-01-14 10:58:50 +0800

    edited signUp func

--- models/user.py
+++ models/user.py
@@ -1,8 +1,36 @@
 from models.base_model import BaseModel
 import peewee as pw
+import re
+from werkzeug.security import generate_password_hash
+
 
 
 class User(BaseModel):
-    email = pw.CharField(unique=True)
-    username = pw.CharField(unique=False)
-    password = pw.CharField()
+    email = pw.CharField(unique=True, null=False)
+    username = pw.CharField(unique=False, null = False)
+    password = pw.CharField(null=False)
+    password = None
+
+    def validate(self):
+        existing_user_email = User.get_or_none(User.email == self.email)
+        if existing_user_email:
+            self.errors.append(f"User with {self.email} already exists")
+
+        existing_user_username = user.get_or_none(User.username == self.username)
+        if existing_user_username:
+            self.errors.append(f"User with {self.username} already exists.")
+
+        #Passwords
+    
+        if len(self.password) <= 6:
+            self.errors.append(f"Password must at least have six characters")
+
+        has_lower = re.search(r"[a-z]", self.password)
+        has_upper = re.search(r"[A-Z]",self.password)
+        has_special = re.search(r"[\[ \] \@ \$ \* \^ \# \%]", self.password)
+
+
+        if has_lower and has_upper and has_special:
+            self.password_hash = generate_password_hash(self.password)
+        else:
+            self.errors.append(f"Password requires changes")
