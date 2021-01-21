162b57f974362eccb3cc9a2c2004eb8c3ee25fdf
Aiman Farhan <aimanfaruk98@gmail.com>
2021-01-21 22:40:40 +0800

    added google login

--- instagram_web/blueprints/sessions/views.py
+++ instagram_web/blueprints/sessions/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 from models.user import User
 from flask_login import login_required, login_user, logout_user
 from werkzeug.security import check_password_hash
-
+from instagram_web.util.google_oauth import oauth
 
 sessions_blueprint = Blueprint('sessions',
                             __name__,
@@ -35,6 +35,24 @@ def create():
     else:
         flash("User does not exist, please try again.")
         return render_template("sessions/new.html")
+
+
+@sessions_blueprint.route("/google_login")
+def google_login():
+    redirect_uri = url_for('sessions.authorize', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+@sessions_blueprint.route("/authorize/google")
+def authorize():
+    oauth.google.authorize_access_token()
+    email = oauth.google.get('https://www.googleapis.com/oauth2/v2/userinfo').json()['email']
+    user = User.get_or_none(User.email == email)
+    if user:
+        login_user(user)
+        return redirect('users.show', username = user.username)
+    else:
+        flash("Google account not found. Please log in with Nextagram username")
+        return redirect('sessions.new')
 
 @sessions_blueprint.route("/logout")
 @login_required
